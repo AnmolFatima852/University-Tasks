7000dfb959c9d6ba09a18b0562feed266b7f7caa
AnmolFatima852 <64316@students.riphah.edu.pk>
2024-11-23 21:12:37 +0500

    Add files via upload

--- 2D ARRAy.cpp
+++ 2D ARRAy.cpp
@@ -2,31 +2,31 @@
 using namespace std;
 int main() 
 {
-    float Entryfee = 100;
-    float Snackcost = 70;
-    string arr[15];
+    float TicketPrice = 50;
+    float PopcornPrice = 80;
+    string arr[10];
     float sum = 0;
-    int max_students = 15;
-    float Totalcost = 0;
+    int max_users = 10;
+    float TotalPrice = 0;
 
-    cout << "How many students  will attend the game night? (Maximum 15)" << endl;
-    cin >> max_students;
+    cout << "How many users will attend the event? (Maximum 10)" << endl;
+    cin >> max_users;
 
-    if (max_students > 15) {
-        cout << "Error!!! Not more students allowed." << endl;
+    if (max_users > 10) {
+        cout << "Error!!! Maximum user limit reached (10)." << endl;
         return 1;
     }
 
-    for (int i = 1; i <=max_students; i++) {
-        cout << "Enter name of students " << i  << ": ";
+    for (int i = 1; i <=max_users; i++) {
+        cout << "Enter name of user " << i  << ": ";
         cin >> arr[i];
 
-        sum = Entryfee + Snackcost;
-        Totalcost += sum;
-        cout << "Students" << i << ":" << arr[i] << "\tTotal Cost: " << sum << endl;
+        sum = TicketPrice + PopcornPrice;
+        TotalPrice += sum;
+        cout << "User" << i << ":" << arr[i] << "\tTotal Price: " << sum << endl;
     }
 
-    cout << "Total cost of entry and snacks: " << Totalcost << endl;
+    cout << "Total Price of all users is: " << TotalPrice << endl;
 
     return 0;
 }
